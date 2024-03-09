e3a6077929284638778bd45d3cf6285ffee14b8e
Kingkalamz <musaogunsolu@gmail.com>
2024-03-09 06:31:43 -0800

    Pycodestyle Hot fix

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -4,6 +4,4 @@ for num1 in range(0, 10):
         if num1 % 10 != 9 or num2 % 10 != 9:
             print("{}{}".format(num1, num2), end=", ")
         else:
-             print("{}{}".format(num1, num2))
-
-
+            print("{}{}".format(num1, num2))
--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -6,8 +6,3 @@ for num1 in range(0, 9):
 
         else:
             print("{}{}".format(num1, num2), end=", ")
-
-
-
-
-
