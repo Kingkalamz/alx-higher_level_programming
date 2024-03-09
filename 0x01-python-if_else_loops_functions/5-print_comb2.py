8a2dde61d0236f308e448f8c2ba087e4620c1e9d
Kingkalamz <musaogunsolu@gmail.com>
2024-03-09 03:01:03 -0800

    00...99

---
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+for num1 in range(0, 10):
+    for num2 in range(0, 10):
+        if num1 % 10 != 9 or num2 % 10 != 9:
+            print("{}{}".format(num1, num2), end=", ")
+        else:
+             print("{}{}".format(num1, num2))
+
+
