7a922003cfd0f791cf4ba725bade6b88a9891e42
Kingkalamz <musaogunsolu@gmail.com>
2024-03-12 14:54:06 -0700

    0...99

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-for num1 in range(0, 10):
-    for num2 in range(0, 10):
-        if num1 % 10 != 9 or num2 % 10 != 9:
-            print("{}{}".format(num1, num2), end=", ")
-        else:
-            print("{}{}".format(num1, num2))
+
+""" prints numbers from 0 to 99 """
+
+for number in range(0, 100):
+    if number == 99:
+        print("{}".format(number))
+    else:
+        print("{:02}".format(number), end=", ")
